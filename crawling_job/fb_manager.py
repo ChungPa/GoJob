8331bf7544ee95ce3c0361c6f731fad0f6c48881
kcrong <kcrong@kim82536.pe.kr>
2016-03-16 00:47:55 +0900

    페이스북 게시물 업로드 추가

---
+++ crawling_job/fb_manager.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+페이스북 Page 포스팅 하는 법 ㅜㅜ 기나긴 삽질 끝에 알아냄 BY KCRONG
+1. 유저에게 manage_pages 와 publish_pages 권한을 요청
+2. https://graph.facebook.com/{page_id}?fields=access_token 으로 '페이지'의 access_token 을 가져옴
+3. 가져온 페이지 access_token 으로,
+    https://graph.facebook.com/{page_id}/feed 에 POST 요청을 보냄.
+    (내용 요청명은 message)
+
+    POST https://graph.facebook.com/{page_id}/feed
+    message = Hello World!
+
+!! 유저의 Access_token 으로 feed 에 요청을 보내면 유저가 페이지에 게시물을 올림.. (소용없)
+
+"""
+import json
+
+from db_config import FACEBOOK_ALARM_USER_ACCESSTOKEN, FACEBOOK_PAGE_ID
+from urllib import urlopen
+import requests
+
+
+def get_page_access_token(user_access_token):
+    url = 'https://graph.facebook.com/%s?fields=access_token&access_token=%s' % (
+        FACEBOOK_PAGE_ID, user_access_token)
+
+    data = json.loads(urlopen(url).read())
+
+    return data['access_token']
+
+
+def write_new_post(message):
+    page_access_token = get_page_access_token(FACEBOOK_ALARM_USER_ACCESSTOKEN)
+
+    url = 'https://graph.facebook.com/%s/feed' % FACEBOOK_PAGE_ID
+    r = requests.post(url, data={
+        'access_token': page_access_token,
+        'message': message
+    })
+
+    print r.text
+    return json.loads(r.text)['id']
+
+
+if __name__ == '__main__':
+    write_new_post("새로운 취직정보!<br>sd\nsdf")
