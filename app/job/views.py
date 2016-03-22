7918e9ccd955e74111b2e7d2a6450f92d3d2a64e
kcrong <kcrong@kim82536.pe.kr>
2016-03-22 15:51:26 +0900

    Add Job Search

--- app/job/views.py
+++ app/job/views.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request
 
 from . import job_blueprint
 from ..models import Job
 
+from werkzeug.exceptions import BadRequest
+
 
 @job_blueprint.route('/')
 def job_list():
-    all_job = Job.query.all()
+    try:
+        query = request.args['query']
+    except BadRequest:
+        query = ""
+
+    all_job = Job.query.filter(Job.title.contains(query) | Job.pay.contains(query) | Job.work_style.contains(query) | Job.role.contains(query) | Job.url.contains(query)).all()
     return render_template('job/worklist.html',
                            all_job=all_job)
